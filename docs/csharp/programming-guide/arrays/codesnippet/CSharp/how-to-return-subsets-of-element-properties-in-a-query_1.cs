1bb1aff50055b787c5c10256bc95e293a5e28d12
UmorenoRamon <36385858+UmorenoRamon@users.noreply.github.com>
2018-03-16 18:02:56 -0400

    Adding students class
    adding students definition

--- docs/csharp/programming-guide/arrays/codesnippet/CSharp/how-to-return-subsets-of-element-properties-in-a-query_1.cs
+++ docs/csharp/programming-guide/arrays/codesnippet/CSharp/how-to-return-subsets-of-element-properties-in-a-query_1.cs
@@ -1,5 +1,20 @@
+                class Student {
+                  public string FirstName;
+                  public string LastName;
+                  public float[] ExamScores;
+	              };
                 private static void QueryByScore()
                 {
+                  	var students = new Student[] {
+                      new Student{ FirstName = "Terry", LastName = "Adams", ExamScores = new float[] {96, 100}}, 
+                      new Student{ FirstName = "Fakhouri", LastName = "Fadi", ExamScores = new float[] {100, 100}}
+                      new Student{ FirstName = "Cesar", LastName = "Garcia", ExamScores = new float[] {100, 100}}
+                      new Student{ FirstName = "Svetlana", LastName = "Omelchenko", ExamScores = new float[] {100, 100}}
+                      new Student{ FirstName = "Eugene", LastName = "Zabokritski", ExamScores = new float[] {100, 100}}
+                      new Student{ FirstName = "John", LastName = "Doe", ExamScores = new float[] {80, 100}}
+              	};
+
+                  
                     // Create the query. var is required because
                     // the query produces a sequence of anonymous types.
                     var queryHighScores =
@@ -12,7 +27,7 @@
                     {
                         // The anonymous type's properties were not named. Therefore 
                         // they have the same names as the Student properties.
-                        Console.WriteLine(obj.FirstName + ", " + obj.LastName);
+                        Console.WriteLine(obj.LastName + ", " + obj.FirstName);
                     }
                 }
                 /* Output:
